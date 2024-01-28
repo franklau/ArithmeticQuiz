fb8f2fd28b0f6e57f682fa0216c5432a5644f752
Frank Lau <franklauvancouver@gmail.com>
2024-01-27 21:04:14 -0800

    remove perform operation

--- Arithmetic/Models/ArithmeticOperation.swift
+++ Arithmetic/Models/ArithmeticOperation.swift
@@ -26,19 +26,6 @@ enum ArithmeticOperation {
     case hard
   }
   
-  func performOperation(lhs: Int, rhs: Int) -> Int {
-    switch self {
-    case .add:
-      return lhs + rhs
-    case .subtract:
-      return lhs - rhs
-    case .multiply:
-      return lhs * rhs
-    case .divide:
-      return lhs / rhs
-    }
-  }
-  
   func getSymbolFor( configuration: UIImage.Configuration, symbolType: SymbolType = .normal) -> UIImage {
     var systemName: String
     switch self {
